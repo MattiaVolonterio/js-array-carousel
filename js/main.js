a39c16422935b6461da5a6daaf3584f8ef173d22
MattiaVolonterio <volonteriomattia@gmail.com>
2024-01-15 19:27:25 +0100

    fix: thumbnail selection

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ const slidesArray = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 let slideHTML = "";
 let thumbHTML = "";
 let slideCount = 0;
+let thumbIndex = 0;
 
 for (i = 0; i < slidesArray.length; i++) {
   const slideName = slidesArray[i];
   let slideCount = i;
+  console.log("ciclo for " + slideCount);
 
   let activeSlide = slideCount == 0 ? "active" : "";
   let activeThumb = slideCount == 0 ? "thumb-active" : "";
@@ -40,7 +42,11 @@ for (i = 0; i < slidesArray.length; i++) {
     oldThumb.classList.remove("thumb-active");
 
     let newThumb = this;
-    let newThumbIndex = this.getAttribute("data-thumb-index");
+    let newThumbIndex = parseInt(this.getAttribute("data-thumb-index"));
+    thumbIndex = newThumbIndex;
+    console.log("premuta immagine " + thumbIndex);
+    slideCount = thumbIndex;
+    console.log(slideCount);
     const newSlide = allSlides[newThumbIndex];
     newSlide.classList.add("active");
 
@@ -54,6 +60,8 @@ slidesContainer.innerHTML = slideHTML;
 // thumbContainer.innerHTML = thumbHTML;
 
 nextButton.addEventListener("click", function () {
+  slideCount = thumbIndex;
+  console.log("tasto premuto prima di incrementare " + slideCount);
   const allSlides = document.getElementsByClassName("slide");
   const allThumb = document.getElementsByClassName("thumb-image");
 
@@ -63,12 +71,14 @@ nextButton.addEventListener("click", function () {
   let currentThumb = document.querySelector(".thumb-image.thumb-active");
   currentThumb.classList.remove("thumb-active");
 
-  if (slideCount < slidesArray.length - 1) {
+  if (thumbIndex < slidesArray.length - 1) {
     slideCount++;
+    thumbIndex++;
   } else {
     slideCount = 0;
+    thumbIndex = 0;
   }
-
+  console.log(slideCount);
   const newSlide = allSlides[slideCount];
   newSlide.classList.add("active");
 
@@ -77,6 +87,7 @@ nextButton.addEventListener("click", function () {
 });
 
 prevButton.addEventListener("click", function () {
+  slideCount = thumbIndex;
   const allSlides = document.getElementsByClassName("slide");
   const allThumb = document.getElementsByClassName("thumb-image");
 
@@ -86,10 +97,12 @@ prevButton.addEventListener("click", function () {
   let currentThumb = document.querySelector(".thumb-image.thumb-active");
   currentThumb.classList.remove("thumb-active");
 
-  if (slideCount > 0) {
+  if (thumbIndex > 0) {
     slideCount--;
+    thumbIndex--;
   } else {
     slideCount = slidesArray.length - 1;
+    thumbIndex = slidesArray.length - 1;
   }
 
   const newSlide = allSlides[slideCount];
